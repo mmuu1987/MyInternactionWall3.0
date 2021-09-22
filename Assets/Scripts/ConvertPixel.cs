ee1f0e653aa12383570ba55658cbdd2f1d1b9c7e
mmuu1987 <262663941@qq.com>
2021-09-22 09:57:31 +0800

    update

---
+++ Assets/Scripts/ConvertPixel.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 把quad 的在3d空间的size转为屏幕尺寸,目前用transform.localscale代表像素尺寸
+/// 默认Z轴的位置为0
+/// </summary>
+public class ConvertPixel : MonoBehaviour
+{
+
+    private Transform _cacheTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _cacheTransform = this.transform;
+
+
+
+        if (Math.Abs(_cacheTransform.localScale.z - 1f) > Mathf.Epsilon)
+        {
+            throw new UnityException("目前仅支持z缩放为1");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Init()
+    {
+        _cacheTransform = this.transform;
+
+
+
+        if (Math.Abs(_cacheTransform.localScale.z - 1f) > Mathf.Epsilon)
+        {
+            throw new UnityException("目前仅支持z缩放为1");
+        }
+    }
+    /// <summary>
+    /// 获取该面片的屏幕尺寸
+    /// </summary>
+    public Rect GetScreenSize()
+    {
+        Vector3 worldPos = _cacheTransform.position;
+
+        Vector3 worldSize = _cacheTransform.localScale;
+
+        //转化到屏幕空间的Rect
+
+        Vector2 screenCenterPos = Camera.main.WorldToScreenPoint(worldPos);
+
+
+
+        Vector2 leftUpPosWorld = worldPos + new Vector3(-worldSize.x / 2, worldSize.y / 2);
+        Vector2 leftUpScreenPos = Camera.main.WorldToScreenPoint(leftUpPosWorld);
+
+
+        Vector2 RightUpPosWorld = worldPos + new Vector3(worldSize.x / 2, worldSize.y / 2);
+        Vector2 RightUpScreenPos = Camera.main.WorldToScreenPoint(RightUpPosWorld);
+
+
+        Vector2 RightDownPosWorld = worldPos + new Vector3(worldSize.x / 2, -worldSize.y / 2);
+        Vector2 RightDownScreenPos = Camera.main.WorldToScreenPoint(RightDownPosWorld);
+
+
+
+        float width = RightUpScreenPos.x - leftUpScreenPos.x;
+
+        float height = RightUpScreenPos.y - RightDownScreenPos.y;
+
+        Rect screenRect = new Rect(screenCenterPos,new Vector2(width,height));
+
+
+
+        return screenRect;
+
+
+
+
+
+    }
+
+    public void SetScreenPos(Vector2 screenPos)
+    {
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, Mathf.Abs(Camera.main.transform.position.z)));
+
+        _cacheTransform.transform.position = worldPos;
+    }
+
+    public void SetScreenSize(Vector2 size)
+    {
+        //先计算屏幕 0 0点在世界坐标的位置
+        Vector2 zeroWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(0f,0f, Mathf.Abs(Camera.main.transform.position.z)));
+
+        //Debug.Log("zeroWorldPos is " +zeroWorldPos);
+        //再计算这个尺寸在世界的位置
+        Vector2 sizeWorldPos = Camera.main.ScreenToWorldPoint(new Vector3(size.x, size.y, Mathf.Abs(Camera.main.transform.position.z)));
+
+        float worldScaleX = Mathf.Abs( sizeWorldPos.x - zeroWorldPos.x);
+
+        float worldScaleY = Mathf.Abs(sizeWorldPos.y - zeroWorldPos.y);
+
+        this._cacheTransform.localScale = new Vector3(worldScaleX,worldScaleY,1);//默认缩放为1
+
+    }
+
+    public void SetPosSize(Rect rect)
+    {
+        SetScreenPos(rect.position);
+
+        SetScreenSize(rect.size);
+    }
+#if UNITY_EDITOR
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(0f, 0f, 100f, 100f), "test"))
+        {
+           // Debug.Log(GetScreenSize().ToString());
+
+           SetScreenPos(new Vector2(1920f,0f));
+
+           SetScreenSize(new Vector2(100, 1080));
+        }
+    }
+
+#endif
+}
---
+++ Assets/Scripts/ConvertPixelMabager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ConvertPixelMabager : MonoBehaviour
+{
+
+    public int ScreenWidth = 1920;
+
+    public int ScreenHeight = 1080;
+
+    public float PictureWidth = 100f;
+
+    public float PictureHeight = 100f;
+
+    public float SpaceWidth = 3f;
+
+    public float SpaceHeight = 3f;
+
+    public int Column = 19;
+
+    public int Row = 10;
+
+
+    private List<ConvertPixel > ConvertPixels = new List<ConvertPixel>();
+
+    /// <summary>
+    /// 选择像素模式还是个数模式,像素模式是每个图片大小一致，个数模式为每个图片的宽不一致，高一致
+    /// </summary>
+    public bool IsSelectPixe = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        HandleData();
+    }
+
+    /// <summary>
+    /// 处理数据，得出图片在屏幕的个数
+    /// </summary>
+    private List<Rect> HandleData()
+    {
+        List<Rect> rects = new List<Rect>();
+        if (IsSelectPixe)
+        {
+            //像素模式下，需要知道屏幕分辨率和Column  row的个数
+
+
+            Column += 1;//列数多一个，防止运动的时候有真空带
+            Row += 1;//横数多一个，防止运动的时候有真空带
+
+            float width = ScreenWidth * 1f / Column ;
+
+            float height = ScreenHeight * 1f / Row;
+
+            for (int i = 0; i < Column; i++)
+            {
+                for (int j = 0; j < Row; j++)
+                {
+                    Rect rect = new Rect(width* i +width/2,height * j+height/2, width,height);
+
+                    rects.Add(rect);
+
+                    GameObject go = GameObject.CreatePrimitive(PrimitiveType.Quad);
+
+                    go.transform.position = Vector3.zero;
+
+                    ConvertPixel cp =  go.AddComponent<ConvertPixel>();
+
+                    go.name = ((i+1) * (j+1)).ToString();
+
+                    cp.Init();
+
+                    ConvertPixels.Add(cp);
+
+                    cp.SetPosSize(rect);
+
+                }
+            }
+        }
+        else
+        {
+            
+        }
+
+        return rects;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        // Draw a semitransparent blue cube at the transforms position
+      //  Gizmos.color = new Color(1, 0, 0, 0.5f);
+        //Gizmos.DrawWireMesh(transform.position, new Vector3(1, 1, 1));
+    }
+}
